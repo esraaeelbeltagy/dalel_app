f2408a6adf5892150ea4c5b7ff9d5f63bea0e291
esraaeelbeltagy <esraae.bltagy19@gmail.com>
2025-02-15 08:43:51 +0200

    Adding Assets To progect

---
+++ lib/core/utils/app_assets.dart
@@ -0,0 +1,19 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  static const String imagesForgetPassword = "assets/images/forget_password.png";
+
+  static const String imagesMosque = "assets/images/mosque.svg";
+
+  static const String imagesOnboarding1 = "assets/images/onboarding_1.png";
+
+  static const String imagesOnboarding2 = "assets/images/onboarding_2.png";
+
+  static const String imagesOnboarding3 = "assets/images/onboarding_3.png";
+
+  static const String imagesOrderSuccess = "assets/images/order_success.png";
+
+  static const String imagesPyramids = "assets/images/pyramids.svg";
+}
+
---
+++ lib/dalel_app.dart
@@ -0,0 +1,17 @@
+
+
+import 'package:flutter/material.dart';
+
+class DalelApp extends StatelessWidget {
+  const DalelApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      appBar: AppBar(
+        title: Text('Dalel App'),
+      ),
+      backgroundColor: Colors.white,
+    );
+  }
+}
